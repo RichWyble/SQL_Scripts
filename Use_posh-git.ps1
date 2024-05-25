821cba17712251c17fdb08addbdbf5c324864762
wybler <rich.wyble@rockymountconsulting.com>
2024-05-25 15:01:14 -0500

    file name changes.  Tweaks to posh-git_experimentation.ps1

---
+++ Use_posh-git.ps1
@@ -0,0 +1,36 @@
+﻿Clear-Host
+# I had to execute this as an administrator
+
+# Check for version... Powershell Core will may come later
+$PSVersion = $PSVersionTable.PSVersion
+if ($PSVersion.Major -lt 5) 
+{
+    "Powershell Version 5.x or Powershell Core 6.0 is needed to run Posh-Git."
+}
+
+#Check Execution policy and adjust as needed.
+#  Consider a method to undo or reset the policy after execution
+$ExecPolicy = Get-ExecutionPolicy
+if ($ExecPolicy -ne "RemoteSigned" -or $ExecPolicy -ne "Unrestricted") 
+{
+    Set-ExecutionPolicy RemoteSigned -Scope CurrentUser -Confirm
+}
+
+#Check for posh-git module.  Install if not present
+$PoshGitMod = Get-Module -ListAvailable -Name posh-git
+if ($PoshGitMod.Name -ne "posh-git")
+{
+    Install-Module posh-git -Scope AllUsers
+}
+
+Get-Module -ListAvailable -Name posh-git
+
+
+
+
+
+
+#reset Execution Policy
+Set-ExecutionPolicy $ExecPolicy -Scope CurrentUser -Confirm
+
+ Get-Module -ListAvailable -Name posh-git
---
+++ posh-git_experimentation.ps1
@@ -0,0 +1,42 @@
+﻿Clear-Host
+
+Import-Module posh-git
+# Need to execute "Install-Module PSColor" as an administrator
+
+cd C:\Repo\RPWData\Scripts
+# will see
+# path [branch  +Added ~Modified -Removed !Conflicted
+#   for more details reference:
+#     https://github.com/dahlbyk/posh-git?tab=readme-ov-file#git-status-summary-information
+# Run git status and capture the output
+#$GitStatus = git status
+
+$Repo = Split-Path -Leaf (git remote get-url origin)
+$UnstagedFiles = git ls-files --others
+$UnstagedFiles
+$UnstagedFiles.count
+
+#.Split([Environment]::NewLine)
+$uf = $UnstagedFiles#.Split(",")# -split ","
+$StagedFiles = git diff --name-only --staged
+
+write-host "Git Status for $Repo"
+Write-Host
+Write-Host "Files Not Under Source Control" -ForegroundColor Red
+foreach ($uf in $UnstagedFiles)
+{
+    Write-Host "  * $uf" -ForegroundColor Red
+}
+Write-Host
+Write-Host "     (use 'git add .' to include all uncommited files be committed)" -ForegroundColor black -BackgroundColor Gray
+foreach ($uf in $UnstagedFiles)
+{
+    Write-Host "     (use 'git add $uf' to include $uf in commit.)" -ForegroundColor black -BackgroundColor Gray
+}
+
+Write-Host
+Write-Host "Staged Files (Under source control, but with uncommitrd changes)" -ForegroundColor Cyan
+Write-Host $StagedFiles -ForegroundColor Cyan
+Write-Host "     (use 'git restore --staged <file>...' to unstage)" -ForegroundColor black -BackgroundColor Gray
+
+                                                                                            
