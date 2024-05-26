cc2c6bb9e5e51c7afeac990ca338127e7cfc37a9
wybler <rich.wyble@rockymountconsulting.com>
2024-05-25 19:17:35 -0500

    MS CoPilot credit.

--- Use_SSMS_Script_Generator_to_Create_Scripts_for_All_Objects_in_a_Database.ps1
+++ Use_SSMS_Script_Generator_to_Create_Scripts_for_All_Objects_in_a_Database.ps1
@@ -1,3 +1,5 @@
+#Created by Microsoft CoPilot
+
 ####In this script, replace 'MY_SERVER_NAME' with your server name and 'MY_DATABASE_NAME' with your database name. This script will generate a SQL script for each table in the specified database and write it to the file C:\output\output.sql1.
 ####
 ####Please note that you need to have the SQL Server Management Studio (SSMS) installed on the machine where you run this script, as it relies on the SMO libraries that come with SSMS1.
